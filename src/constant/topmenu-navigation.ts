f8116dbcbf57b45be4fb7eb4ae0f30870afffc3a
iangelmanuel <iangelmanueldm@gmail.com>
2024-08-15 22:12:57 -0500

    refactor: Add top menu navigation constant
    This commit adds a new file `topmenu-navigation.ts` which includes the `TopMenuNavigation` interface and `topMenuNavigation` constant. These additions provide a centralized location for managing the top menu navigation items. This refactoring improves code organization and maintainability.

---
+++ src/constant/topmenu-navigation.ts
@@ -0,0 +1,19 @@
+interface TopMenuNavigation {
+  title: string
+  url: string
+}
+
+export const topMenuNavigation: TopMenuNavigation[] = [
+  {
+    title: 'Inicio',
+    url: '/'
+  },
+  {
+    title: 'Nosotros',
+    url: '/about'
+  },
+  {
+    title: 'Servicios',
+    url: '/services'
+  }
+]
