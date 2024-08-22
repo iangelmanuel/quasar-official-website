ff5bf8ed748d1efe99dc0f6590364039a1e803bc
iAngelManuel <iangelmanueldm@gmail.com>
2024-08-21 22:57:22 -0500

    refactor: Update shine-pulse animation in tailwind.config.mjs
    This commit updates the 'shine-pulse' animation in the tailwind.config.mjs file. The animation now includes keyframes for 'background-position' to create a pulsating effect. This change enhances the visual appeal of the animation.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -75,6 +75,17 @@ module.exports = {
             transform: 'rotate(215deg) translateX(-500px)',
             opacity: 0
           }
+        },
+        'shine-pulse': {
+          '0%': {
+            'background-position': '0% 0%'
+          },
+          '50%': {
+            'background-position': '100% 100%'
+          },
+          to: {
+            'background-position': '0% 0%'
+          }
         }
       },
       animation: {
